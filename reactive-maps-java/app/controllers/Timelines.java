2e6541778dce00e0aef739f4df4c456e0d46109e
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-04-23 13:26:32 -0600

    start Timelines Controller

--- reactive-maps-java/app/controllers/Timelines.java
+++ reactive-maps-java/app/controllers/Timelines.java
@@ -1,6 +1,8 @@
 package controllers;
 
-
+import play.Logger;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 import models.*;
 
@@ -24,12 +26,39 @@ public class Timelines extends Controller {
         return ok(views.html.index.render(posts));
     }
 
-    public static Result postIndex() throws SQLException
+    public static Result Submit() throws SQLException
     {
         ArrayList<PostModel> posts = new ArrayList<PostModel>();
+        //DynamicForm formData = Form.form().bindFromRequest();
+        DynamicForm.Dynamic formData = Form.form().bindFromRequest().get();
+        Logger.info("hello this is debugging" + formData.getData().toString());
+        /*
+        Logger.debug("hello this is debugging" + formData.getData().toString());
+        Logger.info("Hello from the otter slide");
+        System.out.println("Hello from the otter sliiiiiiide.");
+        //return redirect(controllers.Timelines.index().toString());
+        JavaApplicationDatabase db = new JavaApplicationDatabase();
 
-        System.out.println("aaaaaaaaaaaaaaaa");
-        return ok(views.html.index.render(posts));
+        Connection conn = db.getCxn();
+
+        Statement stmt = conn.createStatement();
+
+
+        //stmt.executeUpdate("create table userInfo (id int, name CHAR(15) );");
+
+        stmt.executeUpdate("INSERT INTO user(id, userName, firstName, lastName, email)VALUES(12, '123456789', '123456789', '123456789', '123456789')");
+
+        ResultSet rs = stmt.executeQuery("SELECT * FROM user");
+
+        boolean row = rs.next();
+        String results = rs.getString("userName");
+        System.out.println(">>>>>>>>>>>"+results.toString());
+
+        rs.close();
+        stmt.close();
+        conn.close();
+*/
+        return redirect("/app");
     }
 
 }
